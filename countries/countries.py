58416920ba35c732df0b1f112cb48173a3a3cba7
Magda <magda.bochinska@gmail.com>
2019-12-22 21:35:44 +0100

    komentarze

--- countries/countries.py
+++ countries/countries.py
@@ -6,6 +6,13 @@ import json
 from json import JSONDecodeError
 
 def request(method, path, post_data: dict=None):
+    """
+    Function downloads all dates from api and decode them
+    :param method: 
+    :param path: 
+    :param post_data: 
+    :return: 
+    """""
     response = requests.request(
         method,
         url=f'{FULL_ROUTE}{path}',
@@ -24,6 +31,12 @@ def request(method, path, post_data: dict=None):
     return content
 
 def country_currencies(all_countries):
+    """
+    Function extracts from content name of country and code of currency.
+    Next creates dict which key is code of country and values are countries.
+    :param all_countries: 
+    :return: 
+    """""
     dict_currency = {}
     for country in all_countries:
         country_name = country['name']
@@ -37,6 +50,12 @@ def country_currencies(all_countries):
     return dict_currency
 
 def create_currency_files(dict_currencies_with_countries, target_path =''):
+    """
+    Function creates the files, which name is code of currency and content are countries.
+    :param dict_currencies_with_countries:
+    :param target_path: 
+    :return:
+    """
     for currency in dict_currencies_with_countries:
         with open(f'{target_path}/{currency}.txt', 'w') as file:
             file.write('\n'.join(dict_currencies_with_countries[currency]))
